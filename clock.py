b11d6c815c738d3cee0f7d4efa05865b9a70f091
Honza <kostohryz.jan@gmail.com>
2023-11-18 19:38:32 +0100

    showing temperature, updated setup mode

--- clock.py
+++ clock.py
@@ -1,17 +1,44 @@
 from microbit import *
 import time
 from math import *
+import speech
 
 
 start_ticks = time.ticks_ms()
-started_at = (23 * 3600) + (52 * 60)
+started_at = (00 * 3600) + (00 * 60)
+
+setup_mode=0
+too_hot = False
+too_cold = False
+
 while True:
-     if button_a.was_pressed():
-          started_at += 60
-     if button_b.was_pressed():
-          started_at -= 60
-     diff = time.ticks_diff(time.ticks_ms(), start_ticks)
-     seconds = diff / 1000 + started_at
-     h = floor(seconds / 3600)
-     m = (seconds - h * 3600) // 60
-     display.scroll(str(h % 24) + ":" + "{:02d}".format(int(m)))
+    if pin_logo.is_touched():
+        setup_mode=time.ticks_ms()
+
+    if (setup_mode+20000)>time.ticks_ms():
+        display.show("S")
+        increment = 60
+        if pin_logo.is_touched():
+              increment = increment*60
+        if button_a.was_pressed():
+              started_at += increment
+        elif button_b.was_pressed():
+              started_at -= increment         
+    else: 
+        diff = time.ticks_diff(time.ticks_ms(), start_ticks)
+        seconds = diff / 1000 + started_at
+        h = floor(seconds / 3600)
+        m = (seconds - h * 3600) // 60
+        display.scroll(str(h % 24) + ":" + "{:02d}".format(int(m)))
+        display.show('-')
+        display.scroll(str(temperature())+'C')
+        if (temperature()>29 and not too_hot):
+            too_hot=True
+            speech.say('Too hot!')
+        elif (temperature()<20 and not too_cold):
+            too_cold=True
+            speech.say('Too cold!')
+        
+        if 20<temperature()<29 :
+            too_cold=False
+            too_hot=False
