2f30a108c5b9261db4534a4892479af896dcc8e9
Honza <kostohryz.jan@gmail.com>
2023-11-21 21:09:28 +0100

    update

--- clock.py
+++ clock.py
@@ -4,41 +4,59 @@ from math import *
 import speech
 
 
-start_ticks = time.ticks_ms()
-started_at = (00 * 3600) + (00 * 60)
+def show_temp(too_hot, too_cold, scroll_delay):
+    if show_temp:
+        display.scroll(str(temperature()) + 'C', delay=scroll_delay)
+        if (temperature() > 29 and not too_hot):
+            too_hot = True
+            speech.say('Too hot!')
+        elif (temperature() < 20 and not too_cold):
+            too_cold = True
+            speech.say('Too cold!')
 
-setup_mode=0
+        if 20 < temperature() < 29:
+            too_cold = False
+            too_hot = False
+    return too_hot, too_cold
+
+
+last_tick = time.ticks_ms()
+abs_last_timestamp = (00 * 3600) + (00 * 60)
+setup_mode = 0
+should_show_temp = False
 too_hot = False
 too_cold = False
-
+scroll_delay = 120
 while True:
     if pin_logo.is_touched():
-        setup_mode=time.ticks_ms()
+        setup_mode = time.ticks_ms()
 
-    if (setup_mode+20000)>time.ticks_ms():
+    if (setup_mode + 30000) > time.ticks_ms():
         display.show("S")
         increment = 60
         if pin_logo.is_touched():
-              increment = increment*60
+            increment = increment * 60
         if button_a.was_pressed():
-              started_at += increment
+            abs_last_timestamp += increment
         elif button_b.was_pressed():
-              started_at -= increment         
-    else: 
-        diff = time.ticks_diff(time.ticks_ms(), start_ticks)
-        seconds = diff / 1000 + started_at
+            abs_last_timestamp -= increment
+    else:
+        if button_a.was_pressed():
+            should_show_temp = not should_show_temp
+        if button_b.was_pressed():
+            if scroll_delay == 120:
+                scroll_delay = 70
+            else:
+                scroll_delay = 120
+
+        now = time.ticks_ms()
+        diff = time.ticks_diff(now, last_tick)
+        last_tick = now
+        seconds = diff / 1000 + abs_last_timestamp
         h = floor(seconds / 3600)
         m = (seconds - h * 3600) // 60
-        display.scroll(str(h % 24) + ":" + "{:02d}".format(int(m)))
-        display.show('-')
-        display.scroll(str(temperature())+'C')
-        if (temperature()>29 and not too_hot):
-            too_hot=True
-            speech.say('Too hot!')
-        elif (temperature()<20 and not too_cold):
-            too_cold=True
-            speech.say('Too cold!')
-        
-        if 20<temperature()<29 :
-            too_cold=False
-            too_hot=False
+        abs_last_timestamp = seconds
+        display.scroll(str(h % 24) + ":" + "{:02d}".format(int(m)), delay=scroll_delay)
+
+        if should_show_temp:
+            too_hot, too_cold = show_temp(too_hot, too_cold, scroll_delay)
